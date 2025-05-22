23b5f9a5f88a07a4a3bdb32e2349af5cd9731a82
Tobias Pfennig <tobiaspfennig@gmx.de>
2025-05-22 18:52:09 +0200

    prepared monte carlo analysis of manually fitted parameters with factor 2

--- Code/robustness_monte_carlo.py
+++ Code/robustness_monte_carlo.py
@@ -34,7 +34,7 @@ n_workers = 100 # Maximum number of parallel threads
 timeout = 600 # Timeout for each thread in seconds
 
 # Set the prefix to be used for logging and results files
-file_prefix = f"montecarlo_allpars_{datetime.now().strftime('%Y%m%d%H%M')}"
+file_prefix = f"montecarlo_manfit_f2_{datetime.now().strftime('%Y%m%d%H%M')}"
 # file_prefix = f"residuals_test"
 
 # Set the random number generator
@@ -49,68 +49,68 @@ email = SMTPMailSender(
 
 # %%
 parameter_ranges = {
-    "PSIItot": (0.5, 2),
-    "PSItot": (0.5, 2),
-    "Q_tot": (0.5, 2),
-    "PC_tot": (0.5, 2),
-    "Fd_tot": (0.5, 2),
-    "NADP_tot": (0.5, 2),
-    "NAD_tot": (0.5, 2),
-    "AP_tot": (0.5, 2),
-    "O2ext": (0.5, 2),
-    "bHi": (0.5, 2),
-    "bHo": (0.5, 2),
-    "cf_lumen": (0.5, 2),
-    "cf_cytoplasm": (0.5, 2),
+    # "PSIItot": (0.5, 2),
+    # "PSItot": (0.5, 2),
+    # "Q_tot": (0.5, 2),
+    # "PC_tot": (0.5, 2),
+    # "Fd_tot": (0.5, 2),
+    # "NADP_tot": (0.5, 2),
+    # "NAD_tot": (0.5, 2),
+    # "AP_tot": (0.5, 2),
+    # "O2ext": (0.5, 2),
+    # "bHi": (0.5, 2),
+    # "bHo": (0.5, 2),
+    # "cf_lumen": (0.5, 2),
+    # "cf_cytoplasm": (0.5, 2),
     "fCin": (0.5, 2),  # manually fitted
-    "kH0": (0.5, 2),
-    "kHst": (0.5, 2),
-    "kF": (0.5, 2),
-    "k2": (0.5, 2),
-    "kPQred": (0.5, 2),
-    "kPCox": (0.5, 2),
-    "kFdred": (0.5, 2),
+    # "kH0": (0.5, 2),
+    # "kHst": (0.5, 2),
+    # "kF": (0.5, 2),
+    # "k2": (0.5, 2),
+    # "kPQred": (0.5, 2),
+    # "kPCox": (0.5, 2),
+    # "kFdred": (0.5, 2),
     "k_F1": (0.5, 2),  # manually fitted
-    "k_ox1": (0.5, 2),
+    # "k_ox1": (0.5, 2),
     "k_Q": (0.5, 2),  # manually fitted
-    "k_NDH": (0.5, 2),
-    "k_SDH": (0.5, 2),
-    "k_FN_fwd": (0.5, 2),
-    "k_FN_rev": (0.5, 2),
+    # "k_NDH": (0.5, 2),
+    # "k_SDH": (0.5, 2),
+    # "k_FN_fwd": (0.5, 2),
+    # "k_FN_rev": (0.5, 2),
     "k_pass": (0.5, 2),  # manually fitted
     "k_aa": (0.5, 2),  # manually fitted
-    "kRespiration": (0.5, 2),
-    "kO2out": (0.5, 2),
-    "kCCM": (0.5, 2),
+    # "kRespiration": (0.5, 2),
+    # "kO2out": (0.5, 2),
+    # "kCCM": (0.5, 2),
     "fluo_influence": (0.5, 2),  # manually fitted
-    "PBS_free": (0.5, 2),
-    "PBS_PS1": (0.5, 2),
-    "PBS_PS2": (0.5, 2),
+    # "PBS_free": (0.5, 2),
+    # "PBS_PS1": (0.5, 2),
+    # "PBS_PS2": (0.5, 2),
     "lcf": (0.5, 2),  # manually fitted
     "KMPGA": (0.5, 2),  # manually fitted
     "kATPsynth": (0.5, 2),  # manually fitted
-    "Pi_mol": (0.5, 2),
-    "HPR": (0.5, 2),
+    # "Pi_mol": (0.5, 2),
+    # "HPR": (0.5, 2),
     "kATPconsumption": (0.5, 2),  # manually fitted
     "kNADHconsumption": (0.5, 2),  # manually fitted
-    "vOxy_max": (0.5, 2),
-    "KMATP": (0.5, 2),
-    "KMNADPH": (0.5, 2),
-    "KMCO2": (0.5, 2),
-    "KIO2": (0.5, 2),
-    "KMO2": (0.5, 2),
-    "KICO2": (0.5, 2),
-    "vCBB_max": (0.5, 2),
-    "kPR": (0.5, 2),
+    # "vOxy_max": (0.5, 2),
+    # "KMATP": (0.5, 2),
+    # "KMNADPH": (0.5, 2),
+    # "KMCO2": (0.5, 2),
+    # "KIO2": (0.5, 2),
+    # "KMO2": (0.5, 2),
+    # "KICO2": (0.5, 2),
+    # "vCBB_max": (0.5, 2),
+    # "kPR": (0.5, 2),
     "kUnquench": (0.5, 2),  # manually fitted
     "KMUnquench": (0.5, 2),  # manually fitted
     "kQuench": (0.5, 2),  # manually fitted
     "KHillFdred": (0.5, 2),  # manually fitted
     "nHillFdred": (0.5, 2),  # manually fitted
-    "k_O2": (0.5, 2),
-    "cChl": (0.5, 2),
-    "CO2ext_pp": (0.5, 2),
-    "S": (0.5, 2),
+    # "k_O2": (0.5, 2),
+    # "cChl": (0.5, 2),
+    # "CO2ext_pp": (0.5, 2),
+    # "S": (0.5, 2),
     "kCBBactivation": (0.5, 2),  # manually fitted
     "KMFdred": (0.5, 2),  # manually fitted
     "kOCPactivation": (0.5, 2),  # manually fitted
@@ -192,7 +192,7 @@ if __name__ == "__main__":
 
     email.send_email(
         body=f"Monte Carlo run {file_prefix} was successfully started",
-        subject=f"Monte Carlo run started"
+        subject="Monte Carlo run started"
     )
 
     # Catch unnecessary warnings:
@@ -245,7 +245,7 @@ if __name__ == "__main__":
 
             email.send_email(
                 body=f"Monte Carlo run {file_prefix} finished successfully. Success: {mcres_outcomes['success'].sum()}, Failed: {mcres_outcomes['failed'].sum()}, Timeout: {mcres_outcomes['timeout'].sum()}",
-                subject=f"Monte Carlo run successful"
+                subject="Monte Carlo run successful"
             )
         
         except Exception as e:
@@ -254,5 +254,5 @@ if __name__ == "__main__":
             
             email.send_email(
                 body=f"Monte Carlo run {file_prefix} encountered an Error:\n{e}",
-                subject=f"Monte Carlo run Error"
+                subject="Monte Carlo run Error"
             )

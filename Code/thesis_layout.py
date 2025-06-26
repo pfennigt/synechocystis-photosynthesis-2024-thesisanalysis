1d7a8e8ab6cd546d99b06a64e3dbccf82517a90a
Tobias Pfennig <tobiaspfennig@gmx.de>
2025-06-26 19:45:57 +0200

    fixed RMSE values, PAM and Biotech plot

--- Code/thesis_layout.py
+++ Code/thesis_layout.py
@@ -146,7 +146,7 @@ thesis_figures["stmodels_analysis_multilight"] = {"main": plt.figure(figsize=fig
 thesis_figures["CO2fix_mono"] = {"main": plt.figure(figsize=figsize(1, 1))}
 
 # Create figure 10
-thesis_figures["Biotech_prod"] = {"main": plt.figure(figsize=figsize(1, 1.5))}
+thesis_figures["Biotech_prod"] = {"main": plt.figure(figsize=figsize(1, 1.2))}
 
 # Create figure 11
 thesis_figures["PAM_singles"] = {"main": plt.figure(figsize=figsize(1, 1))}

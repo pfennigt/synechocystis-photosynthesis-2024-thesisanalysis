412c6acbf05fc6a8cf3f82d9476e83b3de660410
Tobias Pfennig <tobiaspfennig@gmx.de>
2025-07-11 22:56:40 +0200

    further improved plots and quantified results

--- Code/thesis_layout.py
+++ Code/thesis_layout.py
@@ -106,7 +106,7 @@ thesis_figures["CBBcontrol"] = {
 }
 
 # Create figure 2
-thesis_figures["mca_wldifference"] = {"main": plt.figure(figsize=figsize(1, 1))}
+thesis_figures["mca_wldifference"] = {"main": plt.figure(figsize=figsize(1, 0.5))}
 
 # Create figure 3
 thesis_figures["spectra_monochrom"] = {"main": plt.figure(figsize=figsize(1, 0.3))}
@@ -182,15 +182,15 @@ thesis_figures["Overexpression"] = {"main": plt.figure(figsize=figsize(1, 0.8))}
 thesis_figures["Overexpression_mca"] = {"main": plt.figure(figsize=figsize(1, 1))}
 
 # Create figure 22
-fig = plt.figure(constrained_layout=True, figsize=figsize(1, 1.5))
-gs = fig.add_gridspec(3,1, hspace=0.1)
+fig = plt.figure(constrained_layout=True, figsize=figsize(1, 1.2))
+gs = fig.add_gridspec(3,1, hspace=0.05)
 
 sfig1 = fig.add_subfigure(gs[0])
-label_figure(sfig1, pos=(0.1, 0.87), label="A")
+label_figure(sfig1, pos=(0.12, 0.82), label="A")
 sfig2 = fig.add_subfigure(gs[1])
-label_figure(sfig2, pos=(0.1, 0.85), label="B")
+label_figure(sfig2, pos=(0.12, 0.82), label="B")
 sfig3 = fig.add_subfigure(gs[2])
-label_figure(sfig3, pos=(0.1, 0.85), label="C")
+label_figure(sfig3, pos=(0.12, 0.82), label="C")
 
 thesis_figures["Montecarlo_main"] = {
     "main": fig,
@@ -200,42 +200,42 @@ thesis_figures["Montecarlo_main"] = {
 }
 
 # Create figure 23
-thesis_figures["Montecarlo_supp"] = {"main": plt.figure(figsize=figsize(1, 1.5))}
+thesis_figures["Montecarlo_supp"] = {"main": plt.figure(figsize=figsize(1, 1.2))}
 
 # Create figure 24
 thesis_figures["SPint_experiment"]= {"main": plt.figure(figsize=figsize(1, 0.6))}
 
 # Create figure 25
-fig = plt.figure(constrained_layout=True, figsize=figsize(1, 2))
-gs = fig.add_gridspec(4,1, hspace=0.1, height_ratios=(1,1,1,0.2))
+fig = plt.figure(constrained_layout=True, figsize=figsize(1, 1.2))
+gs = fig.add_gridspec(3,1, hspace=0.1, height_ratios=(0.9,0.85,0.75))
 
 sfig1 = fig.add_subfigure(gs[0])
-label_figure(sfig1, pos=(0.1, 0.95), label="A")
+# label_figure(sfig1, pos=(0.1, 0.95), label="A")
 sfig2 = fig.add_subfigure(gs[1])
-label_figure(sfig2, pos=(0.13, 1), label="B")
+# label_figure(sfig2, pos=(0.13, 1), label="B")
 sfig3 = fig.add_subfigure(gs[2])
-label_figure(sfig3, pos=(0.13, 1.02), label="C")
-sfig4 = fig.add_subfigure(gs[3])
-label_figure(sfig4, pos=(0.13, 1.02), label="D")
+# label_figure(sfig3, pos=(0.13, 1.02), label="C")
+# sfig4 = fig.add_subfigure(gs[3])
+# label_figure(sfig4, pos=(0.13, 1.02), label="D")
 
 thesis_figures["Minimize_comparison"] = {
     "main": fig,
     "A": sfig1,
     "B": sfig2,
     "C": sfig3,
-    "D": sfig4,
+    # "D": sfig4,
 }
 
 # Create figure 26
-fig = plt.figure(constrained_layout=True, figsize=figsize(1, 1.5))
-gs = fig.add_gridspec(3,1, hspace=0.1)
+fig = plt.figure(constrained_layout=True, figsize=figsize(1, 1.2))
+gs = fig.add_gridspec(3,1, hspace=0.05)
 
 sfig1 = fig.add_subfigure(gs[0])
-label_figure(sfig1, pos=(0.1, 0.87), label="A")
+label_figure(sfig1, pos=(0.12, 0.82), label="A")
 sfig2 = fig.add_subfigure(gs[1])
-label_figure(sfig2, pos=(0.1, 0.85), label="B")
+label_figure(sfig2, pos=(0.12, 0.82), label="B")
 sfig3 = fig.add_subfigure(gs[2])
-label_figure(sfig3, pos=(0.1, 0.85), label="C")
+label_figure(sfig3, pos=(0.12, 0.82), label="C")
 
 thesis_figures["Montecarlo_main_allpar"] = {
     "main": fig,
@@ -249,3 +249,6 @@ thesis_figures["Montecarlo_supp_allpar"] = {"main": plt.figure(figsize=figsize(1
 
 # Create figure 28
 thesis_figures["spectrum_OCP"] = {"main": plt.figure(figsize=figsize(1, 0.3))}
+
+# Create figure 29
+thesis_figures["Minimize_parameters"] = {"main": plt.figure(figsize=figsize(1, 0.3))}
